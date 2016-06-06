4e1d402a942f8ed825d9d4639d87d8a4acc5696a
dbgdngit <dave.boxall@guardian.co.uk>
2016-06-06 16:18:28 +0100

    add amened models.py and migration files

--- riskdash/riskdashapp/models.py
+++ riskdash/riskdashapp/models.py
@@ -3,24 +3,6 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-
-class Risk(models.Model):
-    riskref = models.CharField(max_length=9)
-    description = models.CharField(max_length=60)
-    owner = models.ForeignKey("Risk_Owners", on_delete=models.CASCADE)
-    created_date = models.DateTimeField('created on')
-    last_updated_date = models.DateTimeField('last updated on')
-    previous_updated_date = models.DateTimeField('previous update')
-    Score = models.IntegerField
-    previous_score = models.IntegerField
-    ranking = models.IntegerField
-    previous_ranking = models.IntegerField
-    Absolute_Likelihood = models.ForeignKey("Likelihood_rating", on_delete=models.CASCADE)
-    Absolute_Impact = models.ForeignKey("Impact_rating", on_delete=models.CASCADE)
-    Rating_rationale = models.CharField(max_length=60)
-    Residual_Likelihood = models.ForeignKey("Res_Likelihood_rating", on_delete=models.CASCADE)
-    Associated_Controls = models.ManyToManyField("Controls")
-
     
 class Risk_type(models.Model):
     description = models.CharField(max_length=15)
@@ -28,31 +10,38 @@ class Risk_type(models.Model):
     def __unicode__(self):
         return self.description
 
+class Control_Owners(models.Model):
+    name = models.CharField(max_length=30,default='')
+
+    def __unicode__(self):
+        return unicode(self.name)
+
+class control_status(models.Model):
+    label = models.CharField(max_length=12,default='')
+
+    def __unicode__(self):
+        return unicode(self.label)
+
 class Controls(models.Model):
     controlref = models.CharField(max_length=9)
-    description = models.CharField(max_length=60)
-    owner = models.ForeignKey("Control_Owners", on_delete=models.CASCADE)
+    description = models.CharField(max_length=100)
+    owner = models.ForeignKey(Control_Owners)
     type = models.CharField(max_length=60)
-    status = models.ForeignKey("control_status", on_delete=models.CASCADE)
+    status = models.ForeignKey(control_status)
     is_pci = models.CharField(max_length=1)
     is_cyber_essentials = models.CharField(max_length=1)
     is_iso27001 = models.CharField(max_length=1)
     is_gdpr = models.CharField(max_length=1)
 
     def __unicode__(self):
-        return self.status
+        return self.description
 
 class Risk_Owners(models.Model):
     name = models.CharField(max_length=30)
 
     def __unicode__(self):
-        return self.name
+        return unicode(self.name)
 
-class Control_Owners(models.Model):
-    name = models.CharField(max_length=30)
-
-    def __unicode__(self):
-        return self.name
 
 class Likelihood_rating(models.Model):
     label = models.CharField(max_length=12)
@@ -75,8 +64,22 @@ class Res_Likelihood_rating(models.Model):
     def __unicode__(self):
         return self.label
 
-class control_status(models.Model):
-    label = models.CharField(max_length=12)
+class Risk(models.Model):
+    riskref = models.CharField(max_length=9)
+    description = models.CharField(max_length=60)
+    owner = models.ForeignKey("Risk_Owners", on_delete=models.CASCADE)
+    created_date = models.DateTimeField('created on')
+    last_updated_date = models.DateTimeField('last updated on')
+    previous_updated_date = models.DateTimeField('previous update')
+    Score = models.IntegerField
+    previous_score = models.IntegerField
+    ranking = models.IntegerField
+    previous_ranking = models.IntegerField
+    Absolute_Likelihood = models.ForeignKey("Likelihood_rating", on_delete=models.CASCADE)
+    Absolute_Impact = models.ForeignKey("Impact_rating", on_delete=models.CASCADE)
+    Rating_rationale = models.CharField(max_length=60)
+    Residual_Likelihood = models.ForeignKey("Res_Likelihood_rating", on_delete=models.CASCADE)
+    Associated_Controls = models.ManyToManyField("Controls")
 
     def __unicode__(self):
-        return self.label
+        return unicode(self.description)
