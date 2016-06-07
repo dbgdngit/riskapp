5e08e092b463af0a381ffb43356e082a3ff18479
dbgdngit <dave.boxall@guardian.co.uk>
2016-06-07 16:11:50 +0100

    fixing path issues preventing startup

--- riskdash/riskdashapp/settings.py
+++ riskdash/riskdashapp/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'riskdash.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/home/dave/riskdash.sqlite3',
+        'NAME': os.path.join('~/riskdash.sqlite3'),
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
