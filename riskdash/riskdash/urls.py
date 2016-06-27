e1ecb6cffd1f770ff7b01409ce26763507f977b0
dbgdngit <dave.boxall@theguardian.com>
2016-06-27 23:23:13 +0100

    added initial view and url

--- riskdash/riskdash/urls.py
+++ riskdash/riskdash/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 
 from django.conf.urls import include, url
 from django.contrib import admin
+from riskdashapp import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     url(r'^riskdashapp/', include('riskdashapp.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- riskdash/riskdashapp/views.py
+++ riskdash/riskdashapp/views.py
@@ -1,7 +1,19 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
-def index(request):
-    return HttpResponse("Risk Dashboard will appear here")
+#def index(request):
+#    return HttpResponse("Risk Dashboard will appear here")
 
 # Create your views here.
+
+from .models import Risk
+
+def index(request):
+    top_risk_list = Risk.objects.order_by('riskref')[:4]
+    template = loader.get_template('index.html')
+    context = {
+        'top_risk_list': top_risk_list,
+    }
+#    output = ', '.join([q.riskref for q in top_risk_list])
+    return HttpResponse(template.render(context, request))
