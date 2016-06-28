e32c1a7385c815344120e4d6da12ae03c373e66a
dbgdngit <dave.boxall@guardian.co.uk>
2016-06-28 17:20:47 +0100

    added sorting and addition field to primary view

--- riskdash/riskdashapp/views.py
+++ riskdash/riskdashapp/views.py
@@ -1,19 +1,17 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-
-#def index(request):
-#    return HttpResponse("Risk Dashboard will appear here")
+import operator
 
 # Create your views here.
 
 from .models import Risk
 
 def index(request):
-    top_risk_list = Risk.objects.order_by('riskref')[:4]
+#    top_risk_list = Risk.objects.order_by('riskref')[:10]
+    top_risk_list = sorted(Risk.objects.all(), key=lambda d: d.Score, reverse=True)
     template = loader.get_template('index.html')
     context = {
         'top_risk_list': top_risk_list,
     }
-#    output = ', '.join([q.riskref for q in top_risk_list])
     return HttpResponse(template.render(context, request))
